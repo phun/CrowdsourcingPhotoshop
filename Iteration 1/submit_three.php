f1627536b37b693e95aadb43654814152bd12bc5
phun <phun@mit.edu>
2012-11-09 11:44:43 -0500

    completed all implementation

--- Iteration 1/submit_three.php
+++ Iteration 1/submit_three.php
@@ -3,14 +3,14 @@ include 'connect_sql.php';
 
 $table_name = "i1_t3";
 
-$ip = $_SERVER['REMOTE_ADDR']; 
-$video = $_GET["video"];
-$beforeIndice = $_GET["beforeIndice"];
-$afterIndice = $_GET["afterIndice"];
-$allBeforeIndices = $_GET["allBeforeIndices"];
-$allAfterIndices = $_GET["allAfterIndices"];
+$ip = mysql_real_escape_string($_SERVER['REMOTE_ADDR']); 
+$video = mysql_real_escape_string($_GET["video"]);
+$beforeIndice = mysql_real_escape_string($_GET["beforeIndice"]);
+$afterIndice = mysql_real_escape_string($_GET["afterIndice"]);
+$allBeforeIndices = mysql_real_escape_string($_GET["allBeforeIndices"]);
+$allAfterIndices = mysql_real_escape_string($_GET["allAfterIndices"]);
 
-echo $beforeIndice . $allBeforeIndices . "<br/.>" . $afterIndice . $allAfterIndices;; 
+echo "Thank you for completing this task!";
 
 $insert_q = "INSERT INTO " . $table_name . " (ip, video, beforeIndice, afterIndice, allBeforeIndices, allAfterIndices)
 	VALUES ('" . $ip . "', '" . $video . "', '" . $beforeIndice . "', '" . $afterIndice . "', '" . $allBeforeIndices . "', '" . $allAfterIndices . "')";
--- Iteration 1/submit_two.php
+++ Iteration 1/submit_two.php
@@ -3,10 +3,12 @@ include 'connect_sql.php';
 
 $table_name = "i1_t2";
 
-$ip = $_SERVER['REMOTE_ADDR']; 
-$video = $_GET["video"];
-$region = $_GET["region"];
-$tool = $_GET["tool"];
+$ip = mysql_real_escape_string($_SERVER['REMOTE_ADDR']); 
+$video = mysql_real_escape_string($_GET["video"]);
+$region = mysql_real_escape_string($_GET["region"]);
+$tool = mysql_real_escape_string($_GET["tool"]);
+
+echo "Thank you for completing this task!";
 
 $insert_q = "INSERT INTO " . $table_name . " (ip, video, region, tool)
 	VALUES ('" . $ip . "', '" . $video . "', '" . $region . "', '" . $tool . "')";
