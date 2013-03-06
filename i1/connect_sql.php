6d3344c81c4b5d9003ea10f086cb8c249d0746fb
phun <phun@mit.edu>
2013-03-06 03:01:04 -0500

    working qualification version

--- i1/connect_sql.php
+++
@@ -1,8 +0,0 @@
- <?php 
- $address = "localhost";
- $user = "inputter";
- $pass = "admin";
- $db_name = "photoshop";
- mysql_connect($address, $user, $pass) or die(mysql_error()); 
- mysql_select_db($db_name) or die(mysql_error()); 
- ?> 
